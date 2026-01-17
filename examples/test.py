c05491a4304376498c467aa040545bb26d861c5a
Hanz <hanz@voyagernetz.com>
2026-01-17 13:59:37 -0600

    updated

---
+++ examples/test.py
@@ -0,0 +1,37 @@
+from typing import TypedDict, Literal
+
+class Attributes(TypedDict):
+    Might: int
+    Agility: int
+    Vitality: int
+    Insight: int
+    Arcana: int
+    Presence: int
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+attributes: Attributes = {
+    "might": 10,
+    "Agility": 12,
+    "Vitality": 14,
+    "Insight": 11,  
+    "Arcana": 13,
+    "Presence": 9
+    }
+
+
